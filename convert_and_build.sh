0d5612d9a51370456202f75614629a9600d80b53
Hugh Delaney <hugh.delaney@codeplay.com>
2022-08-08 11:00:04 +0100

    Add complete convert and build steps

--- convert_and_build.sh
+++ convert_and_build.sh
@@ -4,12 +4,24 @@
 # This work is licensed under the terms of the MIT license.
 # For a copy, see https://opensource.org/licenses/MIT.
 
+set -e
+set -v
+
+SRC_SYCL="src_sycl"
+BUILD_DIR="build"
+
+[[ -e $SRC_SYCL ]] && rm -fr $SRC_SYCL
+[[ -e $BUILD_DIR ]] && rm -fr $BUILD_DIR
+
+cp -r src $SRC_SYCL
+
+cd $SRC_SYCL && dpct *.cu && rm *.cu
+mv dpct_output/* ./ && rmdir dpct_output && cd ..
+cp CMakeLists_dpcpp.txt src_sycl/CMakeLists.txt
+
 sed -i 's/simulator.cuh/simulator.dp.hpp/g' src_sycl/*
 sed -i 's/simulator.cu/simulator.dp.cpp/g' src_sycl/*
 
-BUILD_DIR="build"
-
-rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 cd $BUILD_DIR || exit
 
